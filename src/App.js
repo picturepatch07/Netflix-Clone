b828ac42da74c4cc32c96e8adb9401e40766d4a5
Abhishek Anand <anand2000ys@gmail.com>
2022-06-16 13:28:34 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Row from "./Row";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <Row title="NETFLIX ORIGINALS" />      
+      <Row title="Trending Now" />      
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/requests.js
@@ -0,0 +1,11 @@
+const API_KEY = "ab09f002b311877bb1d78c5563315af5";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+
+}
+
+export default requests;
