36b3c913d500c0629cd971c0398454ff40ecca72
Abhishek Anand <anand2000ys@gmail.com>
2022-06-17 21:05:54 +0530

    Images added

--- src/Row.js
+++ src/Row.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import axios from "./axios";
+import "./Row.css";
+
+const baseURL = "https://image.tmdb.org/t/p/original/";
 
 function Row({title, fetchUrl}) {
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
+      //if [], run once when the row loads, and dont run again
         async function fetchData (){
             const request = await axios.get(fetchUrl);
             setMovies(request.data.results);
@@ -15,11 +19,22 @@ function Row({title, fetchUrl}) {
 
 
   return (
-    <div>
+    <div className="row">
         {/* title */}
         <h2>{title}</h2>
 
+        <div className="rows__posters">
+          {/* several row-posters */}
 
+          {movies.map(movie => (
+            <img 
+            className="row__poster"
+            src={`${baseURL}${movie.poster_path}`} 
+            alt={movie.name} 
+            /> 
+          ))}
+
+        </div>
 
 
         {/* container -> posters */}
