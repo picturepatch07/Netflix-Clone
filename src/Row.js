16e5796aa5256ecaa084e53164f7f1459faa4042
Abhishek Anand <anand2000ys@gmail.com>
2022-06-17 09:52:13 +0530

    Components modified

--- src/Row.js
+++ src/Row.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from "./axios";
 
-function Row({title}) {
-    
+function Row({title, fetchUrl}) {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        async function fetchData (){
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData();
+    }, [fetchUrl]);
 
 
   return (
