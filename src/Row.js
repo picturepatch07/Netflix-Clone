e38163b9f8533cc9ee488803d08765b92fe415c0
Abhishek Anand <anand2000ys@gmail.com>
2022-06-19 00:04:21 +0530

    Netflix Original Row Modified

--- src/Row.js
+++ src/Row.js
@@ -4,7 +4,7 @@ import "./Row.css";
 
 const baseURL = "https://image.tmdb.org/t/p/original/";
 
-function Row({title, fetchUrl}) {
+function Row({title, fetchUrl, isLargeRow}) {
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
@@ -29,8 +29,8 @@ function Row({title, fetchUrl}) {
           {movies.map(movie => (
             <img 
             key={movie.id}
-            className="row__poster"
-            src={`${baseURL}${movie.poster_path}`} 
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${baseURL}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
             alt={movie.name} 
             /> 
           ))}
