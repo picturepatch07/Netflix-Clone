3b23b34dab6b82271131f6e683975045e58bbf93
Abhishek Anand <anand2000ys@gmail.com>
2022-06-25 14:26:15 +0530

    NavBar Added

--- src/Banner.js
+++ src/Banner.js
@@ -51,6 +51,8 @@ function Banner() {
             {truncate(movie?.overview, 150)}
         </h1>
         </div>
+
+        <div className="banner__fadeBottom" />
     </header>
   )
 }
---
+++ src/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div className="nav">
+        <img 
+            className="nav__logo"
+            src="https://www.logo.wine/a/logo/Netflix/Netflix-Logo.wine.svg"
+            alt="Netflix Logo"
+        />
+
+        <img 
+            className="nav_avatar"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkohTJNgr4OxbfK1WgyKVM7ipPiFH4xdKj_w&usqp=CAU"
+            alt="Netflix Logo"
+        />
+    </div>
+  )
+}
+
+export default Nav
